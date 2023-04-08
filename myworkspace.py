3d90b57053078a2ffcf24885567377c5e6d04c25
Eric Ryu <ericryu@Erics-MacBook-Pro.local>
2023-04-08 18:12:18 -0400

    enabled send_person

--- myworkspace.py
+++ myworkspace.py
@@ -13,7 +13,7 @@ class POI():
         bucket = json.loads(bucket)
         self.name = name
         temp_queue = [[] for i in range(500)]
-        self.current_people = temp_queue
+        self.current_people = deque(temp_queue)
         self.visit = visit
         self.bucketed_dwell_time = bucket
         self.same_day_brands = same_day
@@ -54,26 +54,79 @@ class POI():
 
         #self.current_people.append(person)
 
-    def send_person(self, target_poi):
-        if not self.current_people:
-            print("No person available to send.")
-            return
+    def send_person(self, person, poi_dict):
+        #print(self.same_day_brands)
+        instate_sum = 0
+        next_poi_count = 1 #bc outstate is already a part of next poi list
+        outstate_sum = 0
+        outstate_count = 0
+        home_constant = 2
+        next_poi_list = []
 
-        person = self.current_people.popleft()
-        target_poi.add_person(person)
+        for brand_name in self.same_day_brands.keys():
+            if brand_name in poi_dict.keys():
+                instate_sum += self.same_day_brands[brand_name]
+                next_poi_count += 1
+                next_poi_list.append(brand_name)
+            else:
+                outstate_sum += self.same_day_brands[brand_name]
+                outstate_count += 1
+
+        outstate_avg = outstate_sum / outstate_count 
+        next_poi_sum = outstate_avg + instate_sum
+        home_weight = next_poi_sum / next_poi_count
+        home_weight_modified = home_weight / home_constant
+        
+
+        next_poi_list.append("out of state")
+        next_poi_list.append("home")
+
+        #next_poi_list = ['Dollar General', 'out of state', 'home']
+        #final total sum
+        next_poi_sum += home_weight_modified
+        next_poi_weights = []
+        for brand_name in next_poi_list:
+            if brand_name in poi_dict.keys():
+                next_poi_weights.append(self.same_day_brands[brand_name] / next_poi_sum)
+            else:
+                continue
+        
+        next_poi_weights.append(outstate_avg / next_poi_sum)
+        next_poi_weights.append(home_weight_modified / next_poi_sum) 
+       
+
+        next_poi = random.choices(next_poi_list, weights=next_poi_weights)[0]
+
+        # print(instate_sum)
+        # print(outstate_count)
+        # print(outstate_sum)
+        # print(next_poi_count)
+        # print(next_poi_sum)
+        # print(home_weight_modified)
+        print(next_poi_list)
+        print(next_poi_weights)
+        print(next_poi)
+        
+        return [person, next_poi]
+        
 
     #def move_time_step():
 
     #def toString? 
 
-def timestep():
+def timestep(poi_dict):
     '''
         Calculates Each Timestep
     '''
+    #print("timestep 1 result")
+    # for poi in poi_dict.keys():
+    #     print(poi)
+        
 
-def simulation(settings, city_info):
 
-    poi_list = []
+def get_info(city_info):
+
+    poi_dict = {}
 
     for poi_name in city_info:
         visit = city_info[poi_name]['raw_visit_counts']
@@ -83,13 +136,26 @@ def simulation(settings, city_info):
         pop_day = city_info[poi_name]['popularity_by_day']
 
         cur_poi = POI(poi_name, visit, bucket, same_day, pop_hr, pop_day)
-        poi_list.append(cur_poi)
+        poi_dict[poi_name] = cur_poi
 
+    return poi_dict
+
+
+def simulation(settings, city_info):
+
+    poi_dict = get_info(city_info)
+    a = poi_dict["American Heritage Bank"]
+    a.send_person(None, poi_dict)
     #poi all set
 
-if __name__=="__main__":
-    print("main function loading")
+    #TODO delete, test code
 
+    # for i in range(10):
+    #     timestep(poi_dict)
+
+if __name__=="__main__":
+
+    print("main function loading")
 
     with open('simul_settings.yaml', mode="r") as settingstream:
         settings = yaml.full_load(settingstream)
@@ -98,8 +164,7 @@ if __name__=="__main__":
         city_info = yaml.full_load(citystream)
     
     simulation(settings, city_info)
-    a = POI("American Heritage Bank", 39, city_info["American Heritage Bank"]["bucketed_dwell_times"], city_info["American Heritage Bank"]["related_same_day_brand"], city_info["American Heritage Bank"]['popularity_by_hour'], city_info["American Heritage Bank"]['popularity_by_day'])
-    a.add_person()
+
 
 
     
